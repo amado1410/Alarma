1fd060581cb6779061f3b9fb9c535bdf1865f9ab
amado ramos <amado_r_z@hotmail.com>
2018-06-05 09:35:18 -0500

    Update Alarma_con_red_Switch.ino

--- Alarma_con_red_Switch.ino
+++ Alarma_con_red_Switch.ino
@@ -1,40 +1,114 @@
- // Declaramos puertos de entrada y salida asi como las variables
-int led = 13; // Pin asignado al LED
-int reedswitch = 5; // Pin asignado al Reed Switch
-int buzzer =8; // Pin asignado al zumbador
-int val = 0; // Pin asignado para la el valor del reed switch
+ /*
+----------------------------------------------------------------------
+Interruptor magnético - Control de acceso de una puerta
+-----------------------------------------------------------------------
+Programa que genera una alarma con una Pantalla LCD y un Buzzer para 
+emitir un mensaje de alerta y un sonido, a partir de un campo magnético generado a un
+reed switch, logrando disminuir el consumo de energía.
+Sistemas Programables
+Amado Ramos Zuñiga
+15 de marzo del 2018
+*/
 
-void setup(){ // se ejecuta cada vez que el Arduino se inicia
-  pinMode(led,OUTPUT); // El leed se declara como salida
-  pinMode(reedswitch,INPUT); // el Reed Switch se declara como salida
-  pinMode(buzzer,OUTPUT); // El zumbador se declara como salida
-  Serial.begin(9600);
+//--------------------------------------------------
+//Declara librerías a utilizar
+//--------------------------------------------------
+#include <LiquidCrystal.h>    // Incluye la Libreria LiquidCrystal para utilizar la Pantalla LCD
+#include <avr/sleep.h>        // Incluye la Librería AVR/SLEEP para susperder el arduino
+
+//--------------------------------------------------
+//Declara puertos de entradas y salidas y variables
+//--------------------------------------------------
+int contacto = 10;                         // Pin asignado al reed switch
+int buzzer = 11;                           // Pin asignado al Buzzer
+LiquidCrystal lcd(1,2,4,5,6,7);      // Crea un Objeto LC. Parametros: (rs, enable, d4, d5, d6, d7)
+int duracion=250;                         // Duración del sonido
+int fMin=2000;                            // Frecuencia más baja que queremos emitir
+int fMax=4000;                            // Frecuencia más alta que queremos emitir
+int i=0;
+int LED = 13;                             // Parpadeo antes de dormir
+int pinDespertar = 3;                     // pin utilizado para la interrupción
+int contador = 0;
+int tiempo = 10;                          // Variable para el tiempo que durará el sonido de la alarma
+
+// ---------------------------------------------------------------------
+// Método para reproducir el sonido de la alarma mediante las variables de frecuencia más baja y más alta,
+// reproduciendose durante un tiempo determinado
+// ----------------------------------------------------------------------
+void sonido(){
+  for(contador;contador<=tiempo;contador++){
+    //sonido más agudo
+    for (i=fMin;i<=fMax; i++)
+      tone(buzzer, i, duracion);
+    //sonido más grave
+    for (i=fMax;i>=fMin; i--)
+      tone(buzzer, i, duracion);
+  }
+  contador=0;
 }
 
-void loop(){ // Funcion ciclica
-  val = digitalRead(reedswitch); // lee valor en reedswitch
-  // cuando este energizado el Arduino ,
-  // si el iman se acerca a (puerta esta abierta)
-  // reed switch
-  if(val == HIGH){ // si el valor del reed switch esta en 1 
-    //ES DECIR ESTA ENCENDIDO
-    // se repite el ciclo  
-    digitalWrite(led,HIGH); // Se apaga el LED
-    digitalWrite(buzzer,HIGH); // Tambien se apaga el Zumbador
-    delay(200);// Tiempo
-    digitalWrite(led,LOW);//SE APAGA EL LED
-    Serial.println("Se Ha Activado la alarma");//mensaje de la activacion de la alarma
-    digitalWrite(buzzer,LOW);//se apaga el Zumbador
-    delay(200);
-    
-  }
-  // si el iman esta lejos el reed switch La puerta esta cerrada)
-  else{
-    digitalWrite(led,LOW); // Mantiene enciende el LED
-    Serial.println("La Alarma Esta Desactivada");//Mensaje de la alarma desactivada
-    digitalWrite(buzzer,LOW); // Mantiene enciende el zumbador
-   
-    
-  } // Fin del if
-} // Fin del programa
+//------------------------------------
+// Método para manejar la interrupción
+//------------------------------------
+void despertar() {  
+  // ejecuta el código aquí después de la activación antes de volver al ciclo () los temporizadores de la función y el 
+  // código que usa temporizadores (serial.print y más ...) no funcionarán aquí.
+  // en realidad no necesitamos ejecutar ninguna función especial aquí, ya que solo queremos que se despierte  
+}
 
+//------------------------------------
+// Método para dormir el arduino, contribuyendo a disminuir el consumo de energía
+//------------------------------------
+void dormir() {  
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);    // Se establece el modo de bajo consumo  
+    sleep_enable();                         // inicializa el sistema para ponerse en modo bajo consumo pero aun no esta activo  
+    attachInterrupt(0,despertar, LOW);      // usa la interrupción 0 (pin 2) y ejecuta la función. LOW: la interrupción se dispara cuando el pin está en estado LOW.
+    sleep_mode();                           // activa el modo bajo consumo 
+    // EL PROGRAMA CONTINÚA DESDE AQUÍ DESPUÉS DE DESPERTAR 
+    sleep_disable();                        // primera instrucción que debe aparecer cuando se vuelve del modo bajo consumo. desactiva el modo bajo consumo  
+    detachInterrupt(0);                     // Deshabilita la interrupción 0 en el pin 2 por lo que el código despertar no se ejecutará durante el tiempo de ejecución normal.
+}
+
+//------------------------------------
+//Funcion principal
+//------------------------------------
+void setup() {// Se ejecuta cada vez que el Arduino se inicia
+  pinMode(pinDespertar, INPUT);
+  pinMode(LED, OUTPUT);
+  attachInterrupt(0, despertar, LOW);   // usa la interrupción 0 (pin 2) y ejecuta la función wakeUpNow cuando el pin 2 obtiene LOW
+  pinMode(contacto,INPUT);              //El reed switch como una entrada
+  pinMode (buzzer, OUTPUT);             //pin configurado como salida
+  lcd.begin(16,2);                      // Inicializa la interface para el LCD screen, and determina sus dimensiones (ancho y alto) del display                
+  lcd.clear();                          // Limpia la pantalla                  
+}
+
+//------------------------------------
+//Funcion ciclicla
+//------------------------------------
+void loop() // Esta funcion se mantiene ejecutando
+{ // cuando este energizado el Arduino
+  digitalWrite(LED, HIGH);  
+  //delay(1000);  
+  digitalWrite(LED, LOW);    
+  
+  // Si el iman esta lejos del reed switch se activa la alarma
+  if (digitalRead(contacto)==HIGH){
+    lcd.clear();                          // Limpia la pantalla  
+    lcd.setCursor(0,0);                   // setea el cursor a la columna 0, fila 0
+    lcd.print("Control Acceso");          // ingresamos nuestro texto
+    lcd.setCursor(1,1);                   // setea el cursor a la columna 1, fila 1
+    lcd.print("Alerta Intruso!!!");       // imprime mensaje de alerta
+    sonido();
+    delay(50);                            // setea el tiempo del desplazamiento del caracter
+  // Si el iman esta cerca del reed switch se desactiva la alarma
+  }else{
+    lcd.clear();                          // Limpia la pantalla  
+    lcd.setCursor(0,0);                   // setea el cursor a la columna 0, fila 0
+    lcd.print("Control Acceso");          // ingresamos nuestro texto
+    lcd.setCursor(1,1);                   // setea el cursor a la columna 1, fila 1
+    lcd.print("Activado");                // imprime mensaje
+    delay(50);                            // setea el tiempo del desplazamiento del caracter
+  } //Fin del if
+
+  dormir();     // Se llama a la función de dormir
+}
